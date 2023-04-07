06f31501e7713e585727fb1ca66e6c44fcdeb059
Dmitry Shemyakin <68197037+Sh-Dmitry@users.noreply.github.com>
2023-04-07 21:11:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
@@ -7,12 +6,9 @@ from scipy.stats import chi2
 chat_id =  285100540 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    s = (len(x)-1) * np.var(x)
-    scale1 = chi2.ppf(1 - alpha/2, len(x)-1)
-    scale2 = chi2.ppf(alpha/2, len(x)-1)
+    s = (x**2).sum()
+    scale1 = chi2.ppf(1 - alpha/2, len(x))
+    scale2 = chi2.ppf(alpha/2, len(x))
     return np.sqrt(s/(11*scale1)), \
            np.sqrt(s/(11*scale2))

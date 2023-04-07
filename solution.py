51437f40225a36018e65c8dcd1327e14bb1844a8
Dmitry Shemyakin <68197037+Sh-Dmitry@users.noreply.github.com>
2023-04-07 18:00:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
+    alpha = p
     s = (x**2).sum()
     scale1 = chi2.ppf(1 - alpha/2, len(x))
     scale2 = chi2.ppf(alpha/2, len(x))
